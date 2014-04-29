143e380b35a4f86ad362fb154123628962b6c893
Colleen Rock <crockct@mit.edu>
2014-04-29 13:29:41 -0400

    added view of question data from database (currently no dynamic answers, questions not inserted dynamically yet)

--- trans-network.js
+++ trans-network.js
@@ -1,7 +1,10 @@
+// Collections (to model data)
+Messages = new Meteor.Collection("messages");
+Questions = new Meteor.Collection("questions");
+Stories = new Meteor.Collection("stories");
 
+// Routing
 Router.configure({
-
-
 });
 
 Router.map(function() {
@@ -19,10 +22,30 @@ Router.map(function() {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+
+	// Seed Data
+	Questions.insert({title: "I think my partner might be trans*", content: "I think my partner is questioning his gender identity. He is maab and has been talking a lot about how he doesn't feel comfortable with male gender roles, and seems to have some body dysphoria. He is struggling with not conforming to gender roles; he says he feels guilty. I 100% support him no matter what, but as a ciswoman I don't know how he is feeling or what I should do. He hasn't explicitly told me he's questioning, and 							obviously I don't want to push him on this. How can I be most supportive?", topics: "significant others", answers: "0", askedby: "anonymous", askeddate: "", followers: "0"});
+
+
+	Questions.insert({title: "What is the difference between sex and gender?", content: "this is a sample question", topics: "", answers: "0", askedby: "username1", askeddate: "February 1, 2014", followers: "0"});
+
+Questions.insert({title: "Question Title", content: "this is a sample question", topics: "sex and gender, trans* basics", answers: "1", askedby: "anonymous", askeddate: "", followers: "0"});
+
   });
 }
 
 
 if (Meteor.isClient) {
+
+	// all questions
+	Template.questions.all = function () {
+		return Questions.find({});
+	};
+
+	Template.questions.unanswered = function () {
+		return Questions.find({answers: "0"});
+	};
+
+
 
 }
