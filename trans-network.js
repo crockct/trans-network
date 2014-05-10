1ecdc92f0c8b5a9d9b0dd0bd7dddcbe92b3ecee7
Colleen Rock <crockct@mit.edu>
2014-05-10 07:46:38 -0400

    continued making questions more like stories, including adding border

--- trans-network.js
+++ trans-network.js
@@ -40,7 +40,7 @@ if (Meteor.isServer) {
 
 	// Seed Data
 	if (Questions.find().count() === 0) {
-	Questions.insert({title: "I think my partner might be trans*", content: "I think my partner is questioning his gender identity. He is maab and has been talking a lot about how he doesn't feel comfortable with male gender roles, and seems to have some body dysphoria. He is struggling with not conforming to gender roles; he says he feels guilty. I 100% support him no matter what, but as a ciswoman I don't know how he is feeling or what I should do. He hasn't explicitly told me he's questioning, and 							obviously I don't want to push him on this. How can I be most supportive?", topics: "significant others", answers: 0, askedby: "anonymous", askeddate: "", followers: 0, followed: false});
+	Questions.insert({title: "I think my partner might be trans*", content: "I think my partner is questioning his gender identity. He is maab and has been talking a lot about how he doesn't feel comfortable with male gender roles, and seems to have some body dysphoria. He is struggling with not conforming to gender roles; he says he feels guilty. I 100% support him no matter what, but as a ciswoman I don't know how he is feeling or what I should do. He hasn't explicitly told me he's questioning, and obviously I don't want to push him on this. How can I be most supportive?", topics: "significant others", answers: 0, askedby: "anonymous", askeddate: "", followers: 0, followed: false});
 
 
 	Questions.insert({title: "What is the difference between sex and gender?", content: "this is a sample question", topics: "", answers: 0, askedby: "username1", askeddate: "February 1, 2014", followers: 0, followed: false});
