a21f4af8c33b53d7c5d7e78b732a25d0b210392b
Colleen Rock <crockct@mit.edu>
2014-05-14 23:08:29 -0400

    worked on removing pop up message from mentoring page

--- mentoring_yoana.js
+++ mentoring_yoana.js
@@ -5,12 +5,16 @@ if (Meteor.isClient) {
   Template.mentoring.events({
     'click #request_mentor': function(event, template) {
 	
-	$("#request_text").val("");
-	$("#request_text2").val("");
-	//document.getElementById("request_text").innerHTML="";
+		$("#request_text").val("");
+		$("#request_text2").val("");
 		
-		
-		
+		}, 
+
+		'click .pmbtn': function (event, template) {
+		// var button = document.getElementById(event.currentTarget.id);
+		// button.disabled;
+		$("#right_pm_div").show().width("30%").css("position=relative");
+		$("#left_main_div").width("60%").css("float=left", "margin-right=10px");
 		
     }
   });
